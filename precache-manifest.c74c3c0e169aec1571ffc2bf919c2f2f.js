eabe7be9e3dbbded35a6e48218de74d65f708d48
windheart <windheart.me@yandex.ru>
2020-08-24 03:39:02 +0300

    Updates

---
+++ precache-manifest.c74c3c0e169aec1571ffc2bf919c2f2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6ce88207aad512e1fd126e0a89d4cb",
+    "url": "/flyfree/index.html"
+  },
+  {
+    "revision": "52fd1da59d3a60edd125",
+    "url": "/flyfree/static/js/2.9cf2a968.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/flyfree/static/js/2.9cf2a968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0e22587343cf1edcfc",
+    "url": "/flyfree/static/js/main.b374a575.chunk.js"
+  },
+  {
+    "revision": "d66da11cbc4091f8bd58",
+    "url": "/flyfree/static/js/runtime-main.1231bbd8.js"
+  }
+]);
